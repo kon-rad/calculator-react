dcd7681f517eadba770510ae6e8bf07776b1cec5
konrad.gnat <konrad.gnat@webtomed.com>
2019-04-04 14:11:17 -0500

    prog

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import Display from './Display';
-import Keyboard from './Keyboard';
+import Key from './Key';
+
+const OPERATIONS = {
+    '/': 'Divide',
+    'x': 'Multiply',
+    '-': 'Subtract',
+    '+': 'Add',
+    '=': 'Equals'
+};
 
 class Calculator extends Component {
     constructor(props) {
@@ -13,15 +21,66 @@ class Calculator extends Component {
         }
     }
 
+    handleClear() {
+
+    }
+
+    handleDelete() {
+
+    }
+
+    handleOperationAdd(key) {
+
+    }
+
+    handleNewNumber(key) {
+
+    }
+
     keyClickHandler = (key) => {
         console.log(key);
+        if (key === 'CLEAR') {
+            this.handleClear();
+        } else if (key === 'DELETE') {
+            this.handleDelete();
+        } else if (OPERATIONS.hasOwnProperty(key)) {
+            this.handleOperationAdd(key);
+        } else {
+            this.handleNewNumber(key);
+        }
     };
 
     render() {
         return (
             <div className="calculator z-depth-5">
                 <Display displayValue={this.displayValue}/>
-                <Keyboard onKeyClick={this.keyClickHandler}/>
+                <div className="keyboard">
+                    <div className="top_row">
+                        <Key keyType="CLEAR" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="DELETE" onKeyClick={this.keyClickHandler}/>
+                    </div>
+                    <div className="keyboard_operations">
+                        <Key keyType="/" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="x" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="-" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="+" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="=" onKeyClick={this.keyClickHandler}/>
+                    </div>
+                    <div className="keyboard_numbers">
+                        <Key keyType="7" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="8" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="9" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="4" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="5" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="6" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="1" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="2" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="3" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="-" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="0" onKeyClick={this.keyClickHandler}/>
+                        <Key keyType="." onKeyClick={this.keyClickHandler}/>
+                    </div>
+                </div>
             </div>
         );
     }
