40c490fd0d54de426bd37193e0d8410dad218e48
konradgnat <konradmgnat@gmail.com>
2019-04-14 14:19:03 -0500

    Updates

---
+++ precache-manifest.c55eebfdd669c8146777d3c63858889d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec0f49dd31fdf166fb43",
+    "url": "/calculator-react/static/css/main.7d20e9e4.chunk.css"
+  },
+  {
+    "revision": "ec0f49dd31fdf166fb43",
+    "url": "/calculator-react/static/js/main.358cf651.chunk.js"
+  },
+  {
+    "revision": "823ceae628c968c44b0a",
+    "url": "/calculator-react/static/js/runtime~main.b9732b27.js"
+  },
+  {
+    "revision": "aca6dd83b783560439a9",
+    "url": "/calculator-react/static/js/2.eccfb39c.chunk.js"
+  },
+  {
+    "revision": "e14c343d786157a68b82a22d15d411ff",
+    "url": "/calculator-react/index.html"
+  }
+];
